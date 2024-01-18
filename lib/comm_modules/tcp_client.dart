95653f40fbb523952e07c324c11e38fcf870bdb7
Hyunjae <hjstylelu@gmail.com>
2024-01-18 22:18:02 +0900

    2024.01.18 Modify the tcp module

--- lib/comm_modules/tcp_client.dart
+++ lib/comm_modules/tcp_client.dart
@@ -3,12 +3,38 @@ import 'dart:io';
 class TcpClientSocket {
   final String ipAddress = "";
   final int portNo = 0;
-  Socket? client_socket;
+  Socket? _socket;
 
   TcpClientSocket({required ipAddress, required int portNo}) {}
 
-  bool Connect() {
-    return true;
+  Future<void> connect(String serverIP, int serverPort) async {
+    try {
+      _socket = await Socket.connect(serverIP, serverPort);
+      print(
+          'Connected to: ${_socket!.remoteAddress.address}:${_socket!.remotePort}');
+
+      // Send a message to the server
+      _socket!.write('Hello, Server!');
+
+      // Listen for incoming data from the server
+      _socket!.listen(
+        (List<int> data) {
+          String message = String.fromCharCodes(data);
+          print('Received from server: $message');
+        },
+        onDone: () {
+          print('Connection closed by server.');
+          _socket!.destroy();
+        },
+        onError: (error) {
+          print('Error: $error');
+          _socket!.destroy();
+        },
+        cancelOnError: true,
+      );
+    } catch (error) {
+      print('Unable to connect to the server: $error');
+    }
   }
 }
 
